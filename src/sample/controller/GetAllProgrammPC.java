bcf8e12544e6f456f53de406f88da90b2ba4a4a1
“Artem <iachmeneow.a@yandex.ru>
2022-05-04 00:09:16 +0500

    сделал фильтрация строк метода  getAllProgrammPC в классе GetAllProgrammPC

--- src/sample/controller/GetAllProgrammPC.java
+++ src/sample/controller/GetAllProgrammPC.java
@@ -2,6 +2,7 @@ package sample.controller;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class GetAllProgrammPC {
 
@@ -12,7 +13,9 @@ public class GetAllProgrammPC {
 //                    "Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table -AutoSize "
 
     public static void getAllProgrammPC(){ //> С:\list-of-programs.txt
+        ArrayList<String> list=new ArrayList<>();
         try {
+            //> С:\list-of-programs.txt
             String programs_name = " Get-ItemProperty HKLM:\\Software\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\* | " +
                     "Select-Object DisplayName"; //<-- команда для вывода всех программ и сохранения в текстовом файле
             String process_line;
@@ -27,12 +30,32 @@ public class GetAllProgrammPC {
                     new BufferedReader(new InputStreamReader(p.getInputStream()));
             while (((process_line = input.readLine()) != null)) {
 
-                    programs.append(process_line.trim() + "\n");
-
-
-                    // System.out.println(process_line);
+                    list.add(process_line.trim());
 
             }
+
+//            list.remove(0);
+//            list.remove(1);
+//            list.remove(list.size()-2);
+//            list.remove(list.size()-1);
+            //цикл фильтрующий список листа с программами
+            for (int i=0; i<list.size();i++){
+                if ((list.get(i).equals(null))||(list.get(i).equals("\\n"))||(list.get(i).equals(""))||(list.get(i).equals("-----------"))||(list.get(i).contains("DisplayName"))||(list.get(i).equals(""))){
+                    list.set(i,"");
+                }
+              //  System.out.println(list.get(i));
+
+            }
+
+//цикл втсавляющий не пустые строки в список листа с программами
+            for (int i=0; i<list.size();i++){
+                if (!(list.get(i).equals(""))&&!(list.get(i).equals("\n"))&&!(list.get(i).equals("\s"))) {
+                    programs.append(list.get(i) + "\n");
+                }
+            }
+
+
+
 
 
             AllStaticData.app.saveAllProgrammPC(programs);
